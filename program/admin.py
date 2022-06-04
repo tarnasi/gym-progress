23736fdbcac79f7d85b9ba208dec4f7f2b73481c
tarnasi <shahryar.tarnasi@gmail.com>
2022-06-04 13:16:51 +0430

    update models and app

--- program/admin.py
+++ program/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Schedule, Program, Workout
+
+
+@admin.register(Schedule)
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'month', 'start_date', 'id']
+
+
+@admin.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ['name', 'schedule', 'workout_count', 'exersice_per_week', 'id']
+
+
+@admin.register(Workout)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ['name', 'set', 'rep', 'rest', 'last_weight', 'program', 'id']
--- program/models.py
+++ program/models.py
@@ -5,30 +5,47 @@ from .utils import program_images
 
 class Schedule(models.Model):
     YEAR_MONTHS = (
-        ('January', 'january'),
-        ('February', 'february'),
-        ('March', 'march'),
-        ('April', 'april'),
-        ('May', 'may'),
-        ('June', 'june'),
-        ('July', 'july'),
-        ('August', 'august'),
-        ('September', 'september'),
-        ('October', 'october'),
-        ('November', 'november'),
-        ('December', 'december'),
+        ('january', 'january'),
+        ('february', 'february'),
+        ('march', 'march'),
+        ('april', 'april'),
+        ('may', 'may'),
+        ('june', 'june'),
+        ('july', 'july'),
+        ('august', 'august'),
+        ('september', 'september'),
+        ('october', 'october'),
+        ('november', 'november'),
+        ('december', 'december'),
     )
 
     title = models.CharField(max_length=100, blank=True, null=True)
     month = models.CharField(max_length=200, choices=YEAR_MONTHS)
-    start_date = models.DateField()
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
 
 
 class Program(models.Model):
     schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    workout_count = models.PositiveBigIntegerField(default=0)
+    exersice_per_week = models.PositiveBigIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class Workout(models.Model):
+    program = models.ForeignKey(Program, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     set = models.PositiveIntegerField(default=0)
     rep = models.PositiveIntegerField(default=0)
     rest = models.PositiveIntegerField(default=0)
     last_weight = models.CharField(max_length=200, null=True, blank=True)
     image = models.ImageField(upload_to=program_images)
+
+    def __str__(self):
+        return self.name
